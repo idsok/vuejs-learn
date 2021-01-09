2a3745207c5f8039aa94ef3129ec10222ac738dc
Idrissa SOKHONA <i.sokhona84@gmail.com>
2021-01-09 12:37:22 +0100

    apprentissage de composants vue

---
+++ cli-version/src/components/MenuItem.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: "MenuItem",
+  props: ["addToShoppingCart", "image", "inStock", "name", "quantity"]
+};
+</script>
+
+<template>
+  <div class="menu-item">
+    <img
+      class="menu-item__image"
+      :src="image.source"
+      :alt="image.alt"
+    />
+    <div>
+      <h3>{{ name }}</h3>
+      <p v-if="inStock">En stock</p>
+      <p v-else>En rupture de stock</p>
+      <div>
+        <label for="add-item-quantity">Quantité : {{ quantity }}</label>
+        <input
+          id="add-item-quantity"
+          type="number"
+          v-model.number="quantity"
+        />
+        <button @click="addToShoppingCart(quantity)">
+          Ajouter au panier d'achat
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.menu-item {
+  display: flex;
+  width: 500px;
+  justify-content: space-between;
+  margin-bottom: 30px;
+}
+
+.menu-item__image {
+  max-width: 300px;
+}
+</style>
